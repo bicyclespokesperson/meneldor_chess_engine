ad5760a4d3b55ed0532147825697c697a3a69d02
Jeremy Sigrist <jsigrist1@gmail.com>
2022-04-12 20:37:49 -0700

    Add uci command logging

--- app/engine_main.cpp
+++ app/engine_main.cpp
@@ -2,6 +2,35 @@
 #include "senjo/Output.h"
 #include "senjo/UCIAdapter.h"
 
+
+constexpr bool c_log_uci_commands{true};
+
+void log_uci_command(std::string const& cmd)
+{
+  constexpr auto command_log_path = "./command_log.uci";
+
+  static bool first_call{true};
+
+  // Open and close the file on each call so we have a command log in case of a crash
+  std::ofstream command_log{command_log_path, first_call ? std::ofstream::out : std::ofstream::app};
+  if (!command_log.good())
+  {
+      std::cerr << "Failed to open " << command_log_path << " for writing\n";
+      exit(1);
+  }
+
+  if (first_call)
+  {
+    auto const t = std::time(nullptr);
+    auto const tm = *std::localtime(&t);
+    command_log << "Meneldor command log (" << std::put_time(&tm, "%m/%d/%Y %H:%M") << ")\n";
+  }
+  first_call = false;
+
+  command_log << cmd << std::endl;
+  command_log.close();
+}
+
 int main(int argc, char* argv[])
 {
   try
@@ -17,6 +46,11 @@ int main(int argc, char* argv[])
 
     while (std::getline(std::cin, line))
     {
+      if constexpr (c_log_uci_commands)
+      {
+        log_uci_command(line);
+      }
+
       if (!adapter.doCommand(line))
       {
         break;
