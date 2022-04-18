ce6b2ea2c833237083addaffcdbff85583e019dc
Jeremy Sigrist <jsigrist1@gmail.com>
2022-04-17 21:41:19 -0700

    Handle null moves in operator<<

--- src/move.cpp
+++ src/move.cpp
@@ -3,6 +3,12 @@
 std::ostream& operator<<(std::ostream& os, Move const& self)
 {
   // Output move in uci format (e.g. e7e5, or a2a1q for promotions)
+  if (self.type() == Move_type::null)
+  {
+    os << "-";
+    return os;
+  }
+
   os << self.from() << self.to();
   if (self.promotion() != Piece::empty)
   {
